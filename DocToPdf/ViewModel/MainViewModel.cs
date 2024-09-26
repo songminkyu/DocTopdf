26ffc462f198e1c659bd6ed019ed8b128c70fdef
song minkyu <mksong@finaldata.com>
2024-09-26 16:30:12 +0900

    주석 문 추가

--- DocToPdf/ViewModel/MainViewModel.cs
+++ DocToPdf/ViewModel/MainViewModel.cs
@@ -12,6 +12,20 @@ namespace DocToPdf.ViewModel
         public MainViewModel()
         {
             LoggingService.LoggingInit();
+
+
+            /*
+             알림 팝업창 추가할때 아래내용 사용 예정
+
+             타이틀 :               
+             문서 변환 알림
+             
+             본문 : 
+
+             문서 변환 목록이 발견 되었으며, 한글(HWP) 및 PowerPoint(MS Office) 문서가 열려 있습니다. 
+             원활한 문서 변환을 위해 작성 중인 문서를 "저장" 또는 "닫기"를 진행 한 후 "확인" 버튼을 클릭 하여, 
+             문서 변환을 진행 하십시오.
+             */
         }
     }
 }
